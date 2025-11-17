ab8ee63cabbf3672a9500f9900e18e6c24f12aa9
Ariel Reyes <alreyes2@espe.edu.ec>
2025-11-17 09:44:45 -0500

    First commit

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { API_URL } from '../utils/constants';
 import {
-  requestInterceptor,
-  requestErrorInterceptor,
   responseInterceptor,
   responseErrorInterceptor,
 } from './interceptors';
